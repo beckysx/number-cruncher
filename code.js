971d8ae28a6bf0b628193d24afadfba77bf78d35
bexkysx <xu.song@centre.edu>
2019-02-20 23:50:39 -0500

    dfa

--- code.js
+++ code.js
@@ -1,1 +1,19 @@
+var string=document.getElementById("arr")
+var arr=string.innerText.split(',')
 
+arr.forEach(function(num,index){
+  arr[index]=parseInt(arr[index])
+})
+
+var sum =arr.reduce(function(accu,curr){
+
+   return accu+curr
+})
+var mean=sum/arr.length
+
+var show=function(){
+  document.getElementById("mean").innerText= "Mean:"+mean;
+  document.getElementById("mode").innerText= "Mode: 2,6,43";
+  document.getElementById("median").innerText="Median: 18.5";
+
+}
